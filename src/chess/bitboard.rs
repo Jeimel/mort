ce93bae0a43ec67e087a9c5c111544fc56f69af5
Felix Jablinski <jablinskifelix@gmail.com>
2025-07-27 10:21:17 +0200

    Implement `BitOr` for `BitBoard`

--- src/chess/bitboard.rs
+++ src/chess/bitboard.rs
@@ -1,6 +1,16 @@
+use std::ops::BitOr;
+
 /// A `BitBoard` represents a board as array of 64 bits.
 #[repr(transparent)]
 #[derive(Copy, Clone, PartialEq, Eq, Debug, Default)]
 pub struct BitBoard(pub u64);
 
 impl BitBoard {}
+
+impl BitOr for BitBoard {
+    type Output = BitBoard;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        BitBoard(self.0 | rhs.0)
+    }
+}

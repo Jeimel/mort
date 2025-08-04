b033d98a942b3292b17c44269bf07d655dd68322
Felix Jablinski <jablinskifelix@gmail.com>
2025-08-04 09:39:57 +0200

    Implement `BitAnd` and `Not` for `BitBoard`

--- types/src/bitboard.rs
+++ types/src/bitboard.rs
@@ -1,4 +1,4 @@
-use std::ops::BitOr;
+use std::ops::{BitAnd, BitOr, Not};
 
 /// A `BitBoard` represents a board as array of 64 bits.
 #[repr(transparent)]
@@ -14,5 +14,21 @@ impl BitOr for BitBoard {
 
     fn bitor(self, rhs: Self) -> Self::Output {
         BitBoard(self.0 | rhs.0)
+    }
+}
+
+impl BitAnd for BitBoard {
+    type Output = BitBoard;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        BitBoard(self.0 & rhs.0)
+    }
+}
+
+impl Not for BitBoard {
+    type Output = BitBoard;
+
+    fn not(self) -> Self::Output {
+        BitBoard(!self.0)
     }
 }

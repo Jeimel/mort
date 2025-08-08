d3a73646a1ff1a1e81f844bbdf5f1f7170cd39f5
Felix Jablinski <jablinskifelix@gmail.com>
2025-08-08 13:05:56 +0200

    Add `SquareIter` for `BitBoard`

--- types/src/bitboard.rs
+++ types/src/bitboard.rs
@@ -1,4 +1,6 @@
-use std::ops::{BitAnd, BitOr, Not};
+use std::ops::{BitAnd, BitOr, Not, Sub};
+
+use crate::Square;
 
 /// A `BitBoard` represents a board as array of 64 bits.
 #[repr(transparent)]
@@ -7,6 +9,18 @@ pub struct BitBoard(pub u64);
 
 impl BitBoard {
     pub const EMPTY: BitBoard = BitBoard(0);
+
+    pub fn iter(self) -> SquareIter {
+        SquareIter::new(self)
+    }
+}
+
+impl Sub<u64> for BitBoard {
+    type Output = BitBoard;
+
+    fn sub(self, rhs: u64) -> Self::Output {
+        BitBoard(self.0 - rhs)
+    }
 }
 
 impl BitOr for BitBoard {
@@ -30,5 +44,28 @@ impl Not for BitBoard {
 
     fn not(self) -> Self::Output {
         BitBoard(!self.0)
+    }
+}
+
+pub struct SquareIter(BitBoard);
+
+impl SquareIter {
+    pub const fn new(bb: BitBoard) -> Self {
+        Self(bb)
+    }
+}
+
+impl Iterator for SquareIter {
+    type Item = Square;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0 == BitBoard::EMPTY {
+            return None;
+        }
+
+        let lsb = self.0.0.trailing_zeros() as u8;
+        self.0 = self.0 & (self.0 - 1);
+
+        Square::new(lsb)
     }
 }

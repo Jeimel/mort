b441686460f0bc43a480746f261475d02c53da1b
Felix Jablinski <jablinskifelix@gmail.com>
2025-08-10 14:51:57 +0200

    Construct `Color` from either index or fen

--- types/src/color.rs
+++ types/src/color.rs
@@ -6,6 +6,25 @@ pub enum Color {
     Black,
 }
 
+impl Color {
+    pub const fn new(index: u8) -> Option<Self> {
+        if index < 2 {
+            // Safety: `index` has a corresponding `Color` variant
+            Some(unsafe { std::mem::transmute(index) })
+        } else {
+            None
+        }
+    }
+
+    pub fn from_fen(color: &str) -> Self {
+        if color == "w" {
+            Color::White
+        } else {
+            Color::Black
+        }
+    }
+}
+
 impl<T> Index<Color> for [T; 2] {
     type Output = T;
 

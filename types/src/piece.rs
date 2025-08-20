ba3c4ac42406256457f8b81b4b3d922bc89c1110
Felix Jablinski <jablinskifelix@gmail.com>
2025-08-20 23:32:16 +0200

    Constructor and char converter for `PieceType`

--- types/src/piece.rs
+++ types/src/piece.rs
@@ -1,6 +1,7 @@
 use crate::TypeParseError;
 
 #[repr(u8)]
+#[derive(Clone, Copy)]
 pub enum PieceType {
     Pawn,
     Knight,
@@ -8,6 +9,21 @@ pub enum PieceType {
     Rook,
     Queen,
     King,
+}
+
+impl PieceType {
+    pub const fn new(index: u8) -> Option<Self> {
+        if index < 6 {
+            // Safety: `index` has a corresponding `PieceType` variant
+            Some(unsafe { std::mem::transmute(index) })
+        } else {
+            None
+        }
+    }
+
+    pub fn iter() -> impl Iterator<Item = PieceType> {
+        (0..6).map(|index| PieceType::new(index).unwrap())
+    }
 }
 
 impl TryFrom<char> for PieceType {
@@ -22,6 +38,19 @@ impl TryFrom<char> for PieceType {
             'q' => Ok(PieceType::Queen),
             'k' => Ok(PieceType::King),
             _ => Err(TypeParseError::InvalidPieceSymbol),
+        }
+    }
+}
+
+impl From<PieceType> for char {
+    fn from(value: PieceType) -> Self {
+        match value {
+            PieceType::Pawn => 'p',
+            PieceType::Knight => 'n',
+            PieceType::Bishop => 'b',
+            PieceType::Rook => 'r',
+            PieceType::Queen => 'q',
+            PieceType::King => 'k',
         }
     }
 }

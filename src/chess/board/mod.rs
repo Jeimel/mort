7791b4ad8b9e8bff5ee82fb166563c088bbd3f4e
Felix Jablinski <jablinskifelix@gmail.com>
2025-08-21 12:06:03 +0200

    Add piece and color getter to `Board`

--- src/chess/board/mod.rs
+++ src/chess/board/mod.rs
@@ -29,6 +29,49 @@ impl Board {
         *self = Self::EMPTY;
     }
 
+    pub fn all(&self) -> SquareSet {
+        self.colors[Color::White] | self.colors[Color::Black]
+    }
+
+    pub fn color(&self, color: Color) -> SquareSet {
+        self.colors[color]
+    }
+
+    pub fn get(&self, piece: PieceType) -> SquareSet {
+        match piece {
+            PieceType::Pawn => self.pawns(),
+            PieceType::Knight => self.knights(),
+            PieceType::Bishop => self.bishops(),
+            PieceType::Rook => self.rooks(),
+            PieceType::Queen => self.queens(),
+            PieceType::King => self.kings(),
+        }
+    }
+
+    fn pawns(&self) -> SquareSet {
+        self.pawns
+    }
+
+    fn knights(&self) -> SquareSet {
+        self.all() - self.pawns - self.bishops - self.rooks - self.kings()
+    }
+
+    fn bishops(&self) -> SquareSet {
+        self.bishops - self.rooks
+    }
+
+    fn rooks(&self) -> SquareSet {
+        self.rooks - self.bishops
+    }
+
+    fn queens(&self) -> SquareSet {
+        self.rooks & self.bishops
+    }
+
+    fn kings(&self) -> SquareSet {
+        self.kings[Color::White].set() | self.kings[Color::Black].set()
+    }
+
     fn set(&mut self, sq: Square, color: Color, piece: PieceType) {
         self.colors[color].set(sq);
 

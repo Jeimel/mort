23dc5bed92e632149c10ca0308cfd355cd1e9868
Felix Jablinski <jablinskifelix@gmail.com>
2025-10-05 13:22:35 +0200

    Implement perft to validate move generation

--- src/chess/position.rs
+++ src/chess/position.rs
@@ -1,4 +1,6 @@
-use types::Color;
+use std::fmt::Display;
+
+use types::{Color, Move, MoveList};
 
 use crate::uci::Error;
 
@@ -15,6 +17,12 @@ pub struct Position {
     rule50_ply: u8,
 }
 
+impl Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.board)
+    }
+}
+
 impl Position {
     pub fn from_fen(fen: &str) -> Result<Self, Error> {
         let mut board = Board::EMPTY;
@@ -27,4 +35,51 @@ impl Position {
             rule50_ply,
         })
     }
+
+    pub fn make_move(&mut self, mov: Move) -> bool {
+        let stm = self.stm;
+
+        self.stm = !self.stm;
+        self.ply += 1;
+
+        self.board.make_move(mov, stm)
+    }
+
+    pub fn gen_moves(&self) -> MoveList {
+        self.board.gen_moves(self.stm)
+    }
+
+    pub fn perft<const DEBUG: bool>(&self, depth: usize) -> usize {
+        perft::<DEBUG>(&self.board, self.stm, depth)
+    }
+}
+
+fn perft<const ROOT: bool>(board: &Board, stm: Color, depth: usize) -> usize {
+    if depth == 0 {
+        return 1;
+    }
+
+    let mut nodes = 0;
+
+    let moves = board.gen_moves(stm);
+    for i in 0..moves.len() {
+        let mut new_board = *board;
+
+        if new_board.make_move(moves[i], stm) {
+            continue;
+        }
+
+        let child_nodes = perft::<false>(&new_board, !stm, depth - 1);
+        nodes += child_nodes;
+
+        if ROOT {
+            println!("{}: {}", moves[i], child_nodes);
+        }
+    }
+
+    if ROOT {
+        println!("\nNodes searched: {}", nodes);
+    }
+
+    nodes
 }

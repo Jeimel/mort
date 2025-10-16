1ea250a3e70ee802e6dcf7e05cafbea117dba5de
Felix Jablinski <jablinskifelix@gmail.com>
2025-10-16 22:08:09 +0200

    Add draw detection to `Position`

--- src/chess/position.rs
+++ src/chess/position.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-use types::{Color, Move, MoveFlag, MoveList, PieceType};
+use types::{Color, Move, MoveFlag, MoveList, PieceType, SquareSet};
 
 use crate::error::Error;
 
@@ -50,20 +50,52 @@ impl Position {
 
         self.stm = !stm;
         self.ply += 1;
+        self.rule50_ply += 1;
+
         // We reset the fifty move counter if either a pawn moved or a capture has been made
-        self.rule50_ply = if self.board.piece_at(mov.start()) == PieceType::Pawn
-            || mov.flag() == MoveFlag::CAPTURE
-        {
-            0
-        } else {
-            self.rule50_ply + 1
-        };
+        if self.board.piece_at(mov.start()) == PieceType::Pawn || mov.flag() == MoveFlag::CAPTURE {
+            self.rule50_ply = 0;
+        }
 
         self.board.make_move(mov, stm)
     }
 
     pub fn gen_moves(&self) -> MoveList {
         self.board.gen_moves(self.stm)
+    }
+
+    pub fn draw(&self) -> bool {
+        const LIGHT_SQUARES: SquareSet = SquareSet(0x55AA55AA55AA55AA);
+        const DARK_SQUARES: SquareSet = SquareSet(0xAA55AA55AA55AA55);
+
+        if self.rule50_ply >= 100 {
+            return true;
+        }
+
+        let sufficient_material = self.board.get(PieceType::Pawn)
+            | self.board.get(PieceType::Rook)
+            | self.board.get(PieceType::Queen);
+
+        // We can still checkmate with any combination of pawns, rooks, and queens
+        if !(sufficient_material).is_empty() {
+            return false;
+        }
+
+        // We can't checkmate anymore with only one bishops or knight left on the board
+        if (self.board.color(Color::White) | self.board.color(Color::Black)).popcnt() <= 3 {
+            return true;
+        }
+
+        // We can still checkmate with a knight and bishop
+        if !self.board.get(PieceType::Knight).is_empty() {
+            return false;
+        }
+
+        let bishops = self.board.get(PieceType::Bishop);
+
+        // We only have bishops on the board, and the game is drawn
+        // if all of them are on the same color
+        (bishops & LIGHT_SQUARES) == bishops || (bishops & DARK_SQUARES) == bishops
     }
 
     pub fn perft<const DEBUG: bool>(&self, depth: usize) -> usize {

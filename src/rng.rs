283d477833e9339b0c063f10c873e92e82fc59a4
Felix Jablinski <jablinskifelix@gmail.com>
2025-10-19 17:42:35 +0200

    Introduce Zobrist hashing

---
+++ src/rng.rs
@@ -0,0 +1,20 @@
+// Taken from: https://vigna.di.unimi.it/ftp/papers/xorshift.pdf
+pub struct XorShiftState {
+    pub state: u64,
+}
+
+impl XorShiftState {
+    pub const fn new(seed: u64) -> Self {
+        Self { state: seed }
+    }
+
+    pub const fn next(&self) -> (u64, u64) {
+        let mut state = self.state;
+
+        state ^= state >> 12;
+        state ^= state << 25;
+        state ^= state >> 27;
+
+        (state, state.wrapping_mul(2685821657736338717))
+    }
+}

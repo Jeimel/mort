66b4e8e0735d8d1594994ffbf4af9ed1373aa32d
Felix Jablinski <jablinskifelix@gmail.com>
2025-10-22 19:01:24 +0200

    Add macro for constant for-loops

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -7,16 +7,27 @@ mod piece;
 mod rank;
 pub mod slider;
 mod square;
-mod squareset;
+pub mod squareset;
 
 pub use castling::Castling;
 pub use chessmove::{Move, MoveFlag};
 pub use color::Color;
 pub use error::TypeParseError;
 pub use file::File;
-pub use piece::PieceType;
+pub use piece::{Piece, PieceType};
 pub use rank::Rank;
 pub use square::Square;
 pub use squareset::SquareSet;
 
 pub type MoveList = Vec<Move>;
+
+#[macro_export]
+macro_rules! const_for {
+    ($init:stmt; $condition:expr; $next: expr; $body:block) => {
+        $init
+        while $condition {
+            $body;
+            $next;
+        }
+    };
+}

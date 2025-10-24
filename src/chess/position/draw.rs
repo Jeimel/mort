765cc890906d06815122287af654d03a65e9073d
Felix Jablinski <jablinskifelix@gmail.com>
2025-10-24 21:09:46 +0200

    Implement draw detection for `Position`

---
+++ src/chess/position/draw.rs
@@ -0,0 +1,43 @@
+use types::{Color, PieceType, SquareSet};
+
+use super::Position;
+
+impl Position {
+    pub fn draw(&self) -> bool {
+        self.rule50_ply >= 100 || self.insufficient_material()
+    }
+
+    pub fn upcoming_repetition(&self) -> bool {
+        todo!()
+    }
+
+    fn insufficient_material(&self) -> bool {
+        const LIGHT_SQUARES: SquareSet = SquareSet(0x55AA55AA55AA55AA);
+        const DARK_SQUARES: SquareSet = SquareSet(0xAA55AA55AA55AA55);
+
+        let sufficient_material = self.layout.get(PieceType::Pawn)
+            | self.layout.get(PieceType::Rook)
+            | self.layout.get(PieceType::Queen);
+
+        // We can still checkmate with any combination of pawns, rooks, and queens
+        if !sufficient_material.is_empty() {
+            return false;
+        }
+
+        // We can't checkmate anymore with only one bishops or knight left on the board
+        if (self.layout.color(Color::White) | self.layout.color(Color::Black)).popcnt() <= 3 {
+            return true;
+        }
+
+        // We can still checkmate with a knight and bishop
+        if !self.layout.get(PieceType::Knight).is_empty() {
+            return false;
+        }
+
+        let bishops = self.layout.get(PieceType::Bishop);
+
+        // We only have bishops on the board, and the game is drawn
+        // if all of them are on the same color
+        (bishops & LIGHT_SQUARES) == bishops || (bishops & DARK_SQUARES) == bishops
+    }
+}

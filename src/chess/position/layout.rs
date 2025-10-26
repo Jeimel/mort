abfd4b6abf7ff06c539162c6ff1c637c7696b9d3
Felix Jablinski <jablinskifelix@gmail.com>
2025-10-26 21:29:32 +0100

    Refactor `PieceLayout` to add mailbox and threat generation

--- src/chess/position/layout.rs
+++ src/chess/position/layout.rs
@@ -1,14 +1,17 @@
-use types::{Color, PieceType, Square, SquareSet};
+use types::{Color, Piece, PieceType, Square, SquareSet};
 
 use crate::chess::attacks;
 
 #[derive(Clone, Copy)]
 pub struct PieceLayout {
+    // Piece-centric board representation
     pub(super) colors: [SquareSet; 2],
     pub(super) kings: [Square; 2],
     pub(super) rooks: SquareSet,
     pub(super) bishops: SquareSet,
     pub(super) pawns: SquareSet,
+    // Square-centric board representation
+    pub(super) mailbox: [Option<Piece>; 64],
 }
 
 impl PieceLayout {
@@ -18,6 +21,7 @@ impl PieceLayout {
         rooks: SquareSet::EMPTY,
         bishops: SquareSet::EMPTY,
         pawns: SquareSet::EMPTY,
+        mailbox: [None; 64],
     };
 
     pub fn all(&self) -> SquareSet {
@@ -39,26 +43,12 @@ impl PieceLayout {
         }
     }
 
-    pub(crate) fn attacked(&self, sq: Square, stm: Color, occ: SquareSet) -> bool {
-        macro_rules! attacked_by {
-            ($pieces:expr, $attacks:expr) => {
-                let pieces = $pieces & self.color(!stm);
-                if !($attacks & pieces).is_empty() {
-                    return true;
-                }
-            };
+    pub fn piece_at(&self, sq: Square) -> PieceType {
+        if let Some(piece) = self.mailbox[sq] {
+            return piece.typ();
         }
 
-        // We check if there exists an intersection between the given piece-type attacks from `sq`,
-        // and their corresponding pieces on the board. We calculate the attacks based on `sq` to
-        // avoid iterating over every piece, as we are only interested in pieces, which attack `sq`
-        attacked_by!(self.pawns(), attacks::pawn(stm, sq));
-        attacked_by!(self.knights(), attacks::knight(sq));
-        attacked_by!(self.bishops, attacks::bishop(sq, occ));
-        attacked_by!(self.rooks, attacks::rook(sq, occ));
-        attacked_by!(self.kings[!stm].set(), attacks::king(sq));
-
-        false
+        unreachable!()
     }
 
     pub(crate) fn toggle(&mut self, sq: Square, color: Color, piece: PieceType) {
@@ -75,11 +65,33 @@ impl PieceLayout {
             PieceType::Pawn => self.pawns.toggle(sq),
             PieceType::King => self.kings[color] = sq,
             _ => {}
-        }
+        };
+
+        self.mailbox[sq] = match self.mailbox[sq] {
+            Some(_) => None,
+            None => Some(Piece::from(color, piece)),
+        };
     }
 
-    pub(crate) fn king_attacked(&self, color: Color) -> bool {
-        self.attacked(self.kings[color], color, self.all())
+    #[rustfmt::skip]
+    pub(crate) fn attacked(&self, set: SquareSet, color: Color, occ: SquareSet) -> bool {
+        set.iter().all(|sq| self.attackers(sq, color, occ).is_empty())
+    }
+
+    pub(crate) fn attackers(&self, sq: Square, color: Color, occ: SquareSet) -> SquareSet {
+        ((self.pawns & attacks::pawn(color, sq))
+            | (self.knights() & attacks::knight(sq, SquareSet::EMPTY))
+            | (self.bishops & attacks::bishop(sq, occ))
+            | (self.rooks & attacks::rook(sq, occ))
+            | (self.kings[!color].set() & attacks::king(sq, SquareSet::EMPTY)))
+            & self.color(!color)
+    }
+
+    pub(crate) fn snipers(&self, sq: Square, color: Color) -> SquareSet {
+        let rooks = attacks::rook(sq, SquareSet::EMPTY) & self.rooks;
+        let bishops = attacks::bishop(sq, SquareSet::EMPTY) & self.bishops;
+
+        (rooks | bishops) & self.color(!color)
     }
 
     fn pawns(&self) -> SquareSet {

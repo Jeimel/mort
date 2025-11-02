f5b7c730995d7997568bb46dd0d5de69e2b7bf8e
Felix Jablinski <jablinskifelix@gmail.com>
2025-11-02 18:16:48 +0100

    Implement iterative deepening and alpha-beta

--- src/search.rs
+++ src/search.rs
@@ -1,7 +1,100 @@
-use types::Move;
+mod limit;
 
-use crate::Position;
+use crate::{
+    evaluation::{DRAW, INF, evaluate},
+    thread::ThreadData,
+};
 
-pub fn go(_: &mut Position) -> (Move, i32) {
-    todo!()
+pub use limit::SearchLimit;
+use types::MoveList;
+
+pub fn go(thread: &mut ThreadData) {
+    iterative_deepening(thread, thread.limits.depth);
+}
+
+fn iterative_deepening(thread: &mut ThreadData, max_depth: u16) {
+    for depth in 1..=max_depth {
+        let score = alpha_beta(thread, -INF, INF, depth, 0);
+
+        if thread.abort() {
+            break;
+        }
+
+        println!(
+            "info depth {} score cp {} pv {}",
+            depth,
+            score,
+            thread.best.unwrap()
+        );
+
+        thread.score = score;
+    }
+}
+
+fn alpha_beta(thread: &mut ThreadData, mut alpha: i32, beta: i32, depth: u16, ply: i32) -> i32 {
+    if depth <= 0 {
+        return evaluate(&thread.pos);
+    }
+
+    let root = ply == 0;
+
+    // We only check the time constraint on the main search thread
+    if thread.main() {
+        thread.check_limits();
+    }
+
+    if !root && (thread.abort() || thread.pos.draw() || thread.pos.repetition()) {
+        return DRAW;
+    }
+
+    let in_check = thread.pos.check();
+
+    let mut best_score = -INF;
+    let mut best_move = None;
+    let mut legal = 0;
+
+    let mut moves = MoveList::with_capacity(40);
+    thread.pos.generate::<true>(&mut moves);
+
+    for mov in moves {
+        if !thread.pos.legal(mov) {
+            continue;
+        }
+
+        legal += 1;
+
+        thread.pos.make_move(mov);
+        let score = -alpha_beta(thread, -beta, -alpha, depth - 1, ply + 1);
+        thread.pos.unmake_move(mov);
+
+        if score <= best_score {
+            continue;
+        }
+
+        best_score = score;
+
+        if score <= alpha {
+            continue;
+        }
+
+        best_move = Some(mov);
+
+        if score >= beta {
+            break;
+        }
+
+        alpha = score;
+    }
+
+    thread.nodes += legal;
+
+    if root {
+        thread.best = best_move;
+    }
+
+    if legal == 0 {
+        return i32::from(in_check) * (ply - INF);
+    }
+
+    best_score
 }
--- src/thread.rs
+++ src/thread.rs
@@ -5,13 +5,18 @@ use std::{
 
 use types::Move;
 
-use crate::search::SearchLimit;
+use crate::{Position, evaluation::DRAW, search::SearchLimit};
 
 pub struct ThreadData<'a> {
     // Search
     start: Instant,
     abort: &'a AtomicBool,
-    limits: SearchLimit,
+    pub limits: SearchLimit,
+
+    // State
+    pub pos: Position,
+    pub score: i32,
+    main: bool,
 
     // Debug
     pub nodes: u64,
@@ -19,25 +24,32 @@ pub struct ThreadData<'a> {
 }
 
 impl<'a> ThreadData<'a> {
-    pub fn new(abort: &'a AtomicBool, limits: SearchLimit) -> Self {
+    pub fn new(abort: &'a AtomicBool, pos: Position, main: bool, limits: SearchLimit) -> Self {
         Self {
             start: Instant::now(),
             abort,
             limits,
+            pos,
+            score: DRAW,
+            main,
             nodes: 0,
             best: None,
         }
     }
 
     pub fn abort(&self) -> bool {
-        if self.abort.load(Ordering::Relaxed) {
-            return true;
-        }
+        self.abort.load(Ordering::Relaxed)
+    }
 
+    pub fn check_limits(&self) {
         if self.start.elapsed().as_millis() < self.limits.time && self.nodes < self.limits.nodes {
-            return false;
+            return;
         }
 
-        !self.abort.swap(true, Ordering::Relaxed)
+        self.abort.store(true, Ordering::Relaxed);
+    }
+
+    pub fn main(&self) -> bool {
+        self.main
     }
 }

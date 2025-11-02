b444e978bd60a139415cbcf4af12d6d6dfdb6cb5
Felix Jablinski <jablinskifelix@gmail.com>
2025-11-02 18:16:14 +0100

    Implement PeSTO's evaluation

---
+++ src/evaluation.rs
@@ -0,0 +1,36 @@
+mod tables;
+
+use tables::{ENDGAME_TABLE, MIDGAME_TABLE};
+use types::Color;
+
+use crate::Position;
+
+pub const INF: i32 = 30_000;
+pub const DRAW: i32 = 0;
+
+const MIDGAME_VALUE: [i32; 6] = [82, 337, 365, 477, 1025, 0];
+const ENDGAME_VALUE: [i32; 6] = [94, 281, 297, 512, 936, 0];
+
+const PHASE: [i32; 6] = [0, 1, 1, 2, 4, 0];
+
+pub fn evaluate(pos: &Position) -> i32 {
+    let (layout, stm) = (pos.layout(), pos.stm());
+
+    let (mut midgame, mut endgame, mut phase) = ([0, 0], [0, 0], 0);
+
+    for color in [Color::Black, Color::White] {
+        for sq in layout.color(color).iter() {
+            let piece = layout.piece_at(sq);
+
+            let sq = if color == Color::White { sq } else { sq.flip() };
+            midgame[color] += MIDGAME_TABLE[piece][sq] + MIDGAME_VALUE[piece];
+            endgame[color] += ENDGAME_TABLE[piece][sq] + ENDGAME_VALUE[piece];
+
+            phase += PHASE[piece];
+        }
+    }
+
+    phase = phase.min(24);
+
+    ((midgame[stm] - midgame[!stm]) * phase + (endgame[stm] - endgame[!stm]) * (24 - phase)) / 24
+}

b6ec0aab7c0f868820cc4b742e1bde45773bfaa4
Felix Jablinski <jablinskifelix@gmail.com>
2025-11-08 17:18:32 +0100

    Introduce `PrincipalVariation` to store best sequence

---
+++ src/search/pv.rs
@@ -0,0 +1,39 @@
+use std::fmt::Display;
+
+use arrayvec::ArrayVec;
+use types::Move;
+
+use crate::{evaluation::DRAW, search::MAX_DEPTH};
+
+#[derive(Clone)]
+pub struct PrincipalVariation {
+    pub(crate) score: i32,
+    pub(crate) line: ArrayVec<Move, MAX_DEPTH>,
+}
+
+impl Display for PrincipalVariation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for mov in &self.line {
+            write!(f, "{} ", mov)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl PrincipalVariation {
+    pub const EMPTY: Self = Self {
+        score: DRAW,
+        line: ArrayVec::new_const(),
+    };
+
+    pub fn collect(&mut self, mov: Move, score: i32, other: &Self) {
+        self.score = score;
+
+        self.line.clear();
+        self.line.push(mov);
+        self.line
+            .try_extend_from_slice(&other.line)
+            .expect("PV can't be longer than `MAX_DEPTH`");
+    }
+}

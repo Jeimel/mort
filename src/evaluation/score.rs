23d06309080ea68684d66146fea1f1e3becad8cd
Felix Jablinski <jablinskifelix@gmail.com>
2025-11-20 20:15:00 +0100

    Store number of plies from search in `Position`

--- src/evaluation/score.rs
+++ src/evaluation/score.rs
@@ -1,15 +1,15 @@
-use crate::search::MAX_PLY;
+use crate::search::{MAX_DEPTH, MAX_PLY};
 
 pub const INF: i32 = 30_000;
 pub const MATE: i32 = INF - MAX_PLY;
 pub const DRAW: i32 = 0;
 
-pub const fn mate_in(ply: i32) -> i32 {
-    debug_assert!(ply <= MAX_PLY);
-    INF - ply
+pub const fn mate_in(ply: usize) -> i32 {
+    debug_assert!(ply <= MAX_DEPTH);
+    INF - ply as i32
 }
 
-pub const fn mated_in(ply: i32) -> i32 {
-    debug_assert!(ply <= MAX_PLY);
-    -INF + ply
+pub const fn mated_in(ply: usize) -> i32 {
+    debug_assert!(ply <= MAX_DEPTH);
+    -INF + ply as i32
 }

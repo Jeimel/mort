d992ae6f4a5fe84ab2c03e127f4937cf404191e1
Felix Jablinski <jablinskifelix@gmail.com>
2026-09-14 19:11:01 +0200

    Move `TypeParseError` into module file

--- src/chess/types.rs
+++ src/chess/types.rs
@@ -1,7 +1,6 @@
 mod castling;
 mod chessmove;
 mod color;
-mod error;
 mod file;
 mod movelist;
 mod piece;
@@ -13,7 +12,6 @@ mod squareset;
 pub use castling::Castling;
 pub use chessmove::{Move, MoveFlag};
 pub use color::Color;
-pub use error::TypeParseError;
 pub use file::File;
 pub use movelist::{MoveList, MoveListEntry};
 pub use piece::{Piece, PieceType};
@@ -21,3 +19,26 @@ pub use rank::Rank;
 pub use slider::{BISHOP, ROOK, magic};
 pub use square::Square;
 pub use squareset::SquareSet;
+
+use std::{error::Error, fmt::Display};
+
+#[derive(Debug)]
+pub enum TypeParseError {
+    InvalidPieceSymbol(char),
+    InvalidPieceTypeSymbol(char),
+    InvalidColorSymbol(String),
+}
+
+impl Display for TypeParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TypeParseError::InvalidPieceSymbol(symbol) => write!(f, "Invalid symbol: {}", symbol),
+            TypeParseError::InvalidColorSymbol(symbol) => write!(f, "Invalid symbol: {}", symbol),
+            TypeParseError::InvalidPieceTypeSymbol(symbol) => {
+                write!(f, "Invalid symbol: {}", symbol)
+            }
+        }
+    }
+}
+
+impl Error for TypeParseError {}
